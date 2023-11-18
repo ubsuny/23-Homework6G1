d22fb3ed8b82bb8d110896b52b3682e9b481fc9e
James McAllister <94491866+WildJimmy@users.noreply.github.com>
2023-11-18 17:07:19 -0500

    Added calculus.py library

---
+++ calculus/README.txt
@@ -0,0 +1,10 @@
+Make sure when importing this library, the import statement should look like
+
+"import calculus.calculus as calc"
+
+It seems a little redundant but I think the folder structure I have nowis the proper formatting for a library. 
+This makes it so you can call functions like
+"calc.root_simple(...)"
+ 
+If you know the correct formatting to make it so it's just "import calculus" instead of "import calculus.calculus" 
+feel free to let me know  or change it!
---
+++ calculus/calculus.py
@@ -0,0 +1,218 @@
+import math
+import sys
+import cmath
+import numpy as np
+
+
+def simpson(f, a, b, n):
+    """Approximates the definite integral of f from a to b by
+    the composite Simpson's rule, using n subintervals.
+    From http://en.wikipedia.org/wiki/Simpson's_rule
+    """
+    h = (b - a) / n
+    i = np.arange(0, n)
+
+    s = f(a) + f(b)
+    s += 4 * np.sum(f(a + i[1::2] * h))
+    s += 2 * np.sum(f(a + i[2:-1:2] * h))
+
+    return s * h / 3
+
+
+def trapezoid(f, a, b, n):
+    """Approximates the definite integral of f from a to b by
+    the composite trapezoidal rule, using n subintervals.
+    From http://en.wikipedia.org/wiki/Trapezoidal_rule
+    """
+    h = (b - a) / n
+    s = f(a) + f(b)
+    i = np.arange(0, n)
+    s += 2 * np.sum(f(a + i[1:] * h))
+    return s * h / 2
+
+
+def adaptive_trapezoid(f, a, b, acc, output=False):
+    """
+    Uses the adaptive trapezoidal method to compute the definite integral
+    of f from a to b to desired accuracy acc.
+    """
+    old_s = np.inf
+    h = b - a
+    n = 1
+    s = (f(a) + f(b)) * 0.5
+    if output == True:
+        print("N = " + str(n + 1) + ",  Integral = " + str(h * s))
+    while abs(h * (old_s - s * 0.5)) > acc:
+        old_s = s
+        for i in np.arange(n):
+            s += f(a + (i + 0.5) * h)
+        n *= 2.0
+        h *= 0.5
+        if output == True:
+            print("N = " + str(n) + ",  Integral = " + str(h * s))
+    return h * s
+
+
+def root_print_header(algorithm, accuracy):
+    sys.stdout.write(
+        "\n ROOT FINDING using "
+        + algorithm
+        + "\n Requested accuracy = "
+        + repr(accuracy)
+        + "\n Step     Guess For Root          Step Size      "
+        + "     Function Value"
+        + "\n ----  --------------------  --------------------"
+        + "  --------------------"
+        + "\n"
+    )
+
+
+def root_print_step(step, x, dx, f_of_x):
+    sys.stdout.write(repr(step).rjust(5))
+    for val in [x, dx, f_of_x]:
+        sys.stdout.write("  " + repr(val).ljust(20))
+    sys.stdout.write("\n")
+
+
+def root_max_steps(algorithm, max_steps):
+    raise Exception(
+        " " + algorithm + ": maximum number of steps " + repr(max_steps) + " exceeded\n"
+    )
+
+
+def root_simple(f, x, dx, accuracy=1.0e-6, max_steps=1000, root_debug=False):
+    """Return root of f(x) given guess x and step dx with specified accuracy.
+    Step must be in direction of root: dx must have same sign as (root - x).
+    """
+    f0 = f(x)
+    fx = f0
+    step = 0
+    iterations = []
+    if root_debug:
+        root_print_header("Simple Search with Step Halving", accuracy)
+        root_print_step(step, x, dx, f0)
+        iterations.append([x, f0])
+    while abs(dx) > abs(accuracy) and f0 != 0.0:
+        x += dx
+        fx = f(x)
+        if f0 * fx < 0.0:  # stepped past root
+            x -= dx  # step back
+            dx /= 2.0  # use smaller step
+        step += 1
+        if step > max_steps:
+            root_max_steps("root_simple", max_steps)
+        if root_debug:
+            root_print_step(step, x, dx, fx)
+            iterations.append([x, fx])
+    return x, np.array(iterations)
+
+
+def root_bisection(f, x1, x2, accuracy=1.0e-6, max_steps=1000, root_debug=False):
+    """Return root of f(x) in bracketed by x1, x2 with specified accuracy.
+    Assumes that f(x) changes sign once in the bracketed interval.
+    Uses bisection root-finding algorithm.
+    """
+    iterations = []
+    f1 = f(x1)
+    f2 = f(x2)
+    if f1 * f2 > 0.0:
+        raise Exception("f(x1) * f(x2) > 0.0")
+    x_mid = (x1 + x2) / 2.0
+    f_mid = f(x_mid)
+    dx = x2 - x1
+    step = 0
+    if root_debug:
+        iterations = []
+        root_print_header("Bisection Search", accuracy)
+        root_print_step(step, x_mid, dx, f_mid)
+        iterations.append([x_mid, f_mid])
+    while abs(dx) > accuracy:
+        if f_mid == 0.0:
+            dx = 0.0
+        else:
+            if f1 * f_mid > 0:
+                x1 = x_mid
+                f1 = f_mid
+            else:
+                x2 = x_mid
+                f2 = f_mid
+            x_mid = (x1 + x2) / 2.0
+            f_mid = f(x_mid)
+            dx = x2 - x1
+        step += 1
+        if step > max_steps:
+            warning = "Too many steps (" + repr(step) + ") in root_bisection"
+            raise Exception(warning)
+        if root_debug:
+            root_print_step(step, x_mid, dx, f_mid)
+            iterations.append([x_mid, f_mid])
+    return x_mid, np.array(iterations)
+
+
+def root_secant(f, x0, x1, accuracy=1.0e-6, max_steps=20, root_debug=False):
+    """Return root of f(x) given guesses x0 and x1 with specified accuracy.
+    Uses secant root-finding algorithm.
+    """
+    iterations = []
+    f0 = f(x0)
+    dx = x1 - x0
+    step = 0
+    if root_debug:
+        root_print_header("Secant Search", accuracy)
+        root_print_step(step, x0, dx, f0)
+        iterations.append([x0, f0])
+    if f0 == 0:
+        return x0
+    while abs(dx) > abs(accuracy):
+        f1 = f(x1)
+        if f1 == 0:
+            return x1
+        if f1 == f0:
+            raise Exception("Secant horizontal f(x0) = f(x1) algorithm fails")
+        dx *= -f1 / (f1 - f0)
+        x0 = x1
+        f0 = f1
+        x1 += dx
+        step += 1
+        if step > max_steps:
+            root_max_steps("root_secant", max_steps)
+        if root_debug:
+            root_print_step(step, x1, dx, f1)
+            iterations.append([x1, f1])
+    return x1, np.array(iterations)
+
+
+def root_tangent(f, fp, x0, accuracy=1.0e-6, max_steps=20, root_debug=False):
+    """Return root of f(x) with derivative fp = df(x)/dx
+    given initial guess x0, with specified accuracy.
+    Uses Newton-Raphson (tangent) root-finding algorithm.
+    """
+    iterations = []
+    f0 = f(x0)
+    fp0 = fp(x0)
+    if fp0 == 0.0:
+        raise Exception(" root_tangent df/dx = 0 algorithm fails")
+    dx = -f0 / fp0
+    step = 0
+    if root_debug:
+        root_print_header("Tangent Search", accuracy)
+        root_print_step(step, x0, dx, f0)
+        iterations.append([x0, f0])
+    if f0 == 0.0:
+        return x0
+    while True:
+        fp0 = fp(x0)
+        if fp0 == 0.0:
+            raise Exception(" root_tangent df/dx = 0 algorithm fails")
+        dx = -f0 / fp0
+        x0 += dx
+        f0 = f(x0)
+        if abs(dx) <= accuracy or f0 == 0.0:
+            return x0
+        step += 1
+        if step > max_steps:
+            root_max_steps("root_tangent", max_steps)
+        if root_debug:
+            root_print_step(step, x0, dx, f0)
+            iterations.append([x0, f0])
+    return x0
