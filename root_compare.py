870b1dd2fb2f7174371d2ce490fbcf8bf6f4606e
James McAllister <94491866+WildJimmy@users.noreply.github.com>
2023-11-18 19:15:26 -0500

    Added Accuracy Calculating Function

--- root_compare.py
+++ root_compare.py
@@ -18,7 +18,7 @@ def compare_steps(f_input):
 
     # Note we need the root_debug = True to get values in our ierations array, which
     # we then take the length of to get the number of steps for root finding
-    steps_simple = len(calc.root_simple(f_input, start, 0.5, root_debug=True)[1])
+    steps_simple = len(calc.root_simple(f_input, start, 0.1, root_debug=True)[1])
 
     steps_secant = len(
         calc.root_secant(f_input, start, np.random.rand() * start, root_debug=True)[1]
@@ -26,6 +26,22 @@ def compare_steps(f_input):
 
     return steps_simple, steps_secant
 
+def Compare_accuracy(f_input, true_root, step_max):
+
+    # Gives a random number between -2, 2
+    start = (np.random.rand() - 0.5) * 4
+
+    # Note we need the root_debug = True to get values in our ierations array, which
+    # we then take the length of to get the number of steps for root finding
+    rt_simple = calc.root_simple(f_input, start, 0.1, max_steps = step_max)[0]
+
+    rt_secant = calc.root_secant(f_input, start, np.random.rand() * start, max_steps = step_max)[0]
+    
+    acc_simple = rt_simple - true_root
+    
+    acc_secant = rt_secant - true_root
+
+    return acc_simple, acc_secant
 
 def average_steps(f_input, trials):
 
@@ -42,7 +58,6 @@ def average_steps(f_input, trials):
     avg_secant = np.average(secant_array)
 
     return avg_simple, avg_secant
-
 
 simp_avg, sec_avg = average_steps(np.tan, 500)
 print(
